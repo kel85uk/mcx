afdc9023f4f61d7d574849e80d4526490aa67599
kel85uk <kel85uk@gmail.com>
2017-10-18 23:29:31 +0200

    Added the phase function

--- src/mcx_phase.h
+++ src/mcx_phase.h
@@ -1,3 +1,8 @@
+/**
+  This header file is added to enable the barest minimum for organizing the code. This file is used 
+  to modify the phase function. The user should return a variable theta.
+**/
+
 //Henyey-Greenstein Phase Function, "Handbook of Optical 
                        //Biomedical Diagnostics",2002,Chap3,p234, also see Boas2002
 
